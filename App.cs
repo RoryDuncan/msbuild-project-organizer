aae801e918c3ec503a747136f839015dc735a5d0
Rory Duncan <robust.rory@gmail.com>
2020-10-05 00:26:42 -0700

    Add document saving

--- App.cs
+++ App.cs
@@ -5,6 +5,8 @@ using csproj_sorter.Services;
 using System.IO;
 using System.Xml.Linq;
 using csproj_sorter.Enums;
+using System.Xml;
+using System.Text;
 
 namespace csproj_sorter
 {
@@ -36,23 +38,40 @@ namespace csproj_sorter
             // todo: read json as SortSettings class
         }
 
-        public void Run(string target)
+        public void Run(string input)
         {
-            _logger.LogInformation($"params: {target}");
-            if (string.IsNullOrWhiteSpace(target)) 
+            _logger.LogInformation($"params: {input}");
+            if (string.IsNullOrWhiteSpace(input)) 
             {
                 _logger.LogError($"A --filename was not provided. Run this application with the name of a .csproj file as the --filename arg.");
                 return;
             }
 
-            _logger.LogInformation($"Sorting '{target}'...");
+            _logger.LogInformation($"Sorting '{input}'...");
 
-            XDocument document = _xmlService.GetXML(target);
+            XDocument document = _xmlService.GetXML(input);
             _groupingService.Group(document, GroupBy.NodeType);
-            
+            _logger.LogInformation("Sort complete.");
 
-            _testService.Run();
+            SaveDocument($"{input}.sorted", document);
+
+            // _testService.Run();
         }
 
+        public void SaveDocument(string fileName, XDocument document)
+        {
+            _logger.LogInformation($"Saving Document as {fileName}.");
+
+            var settings = new XmlWriterSettings()
+            {
+                Indent = true,
+            };
+
+            using (XmlWriter writer = XmlWriter.Create(fileName, settings))
+            {
+                document.Save(writer);
+            }
+            _logger.LogInformation("Done.");
+        }
     }
 }
