0af819fb8fdfafcf45c0bc20ce59e35b49162131
Rory Duncan <robust.rory@gmail.com>
2020-10-11 11:53:13 -0700

    Add application as reference to test

---
+++ CSProjOrganizer.Tests/XMLServiceTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace CSProjOrganizer.Tests
+{
+    public class XMLServiceTests
+    {
+
+        private readonly IXmlService _xmlService;
+
+        public XMLServiceTests()
+        {
+
+        }
+        [Fact]
+        public void CanLoadFile()
+        {
+
+        }
+    }
+}
