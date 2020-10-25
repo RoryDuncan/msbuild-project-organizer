69d957716a7878e68c0bd84d650946ae46c9b04b
Rory Duncan <robust.rory@gmail.com>
2020-10-25 13:37:00 -0700

    Add xmldoc

--- src/csproj-organizer/services/XmlNameProvider.cs
+++ src/csproj-organizer/services/XmlNameProvider.cs
@@ -3,27 +3,40 @@ using CSProjOrganizer.Interfaces;
 
 namespace CSProjOrganizer.Services
 {
+    /// <summary>
+    /// An implementation of IXmlNameProvider
+    /// </summary>
     public class XmlNameProvider : IXmlNameProvider
     {
         private XNamespace _xmlns;
 
+        /// <summary>
+        /// Create an XmlNameProvider with the XML namespace context
+        /// </summary>
+        /// <param name="xmlns">The XML namespace that should be used by all other methods</param>
         public XmlNameProvider(XNamespace xmlns)
         {
             _xmlns = xmlns;
         }
 
+        /// <inheritdoc />
         public XName Get(string name)
         {
             return _xmlns.GetName(name);
         }
-
+        
+        /// <inheritdoc />
         public XName ItemGroup => Get("ItemGroup");
+        /// <inheritdoc />
         public XName Project => Get("Project");
 
-        // Attributes don't need scoping to an XMLNS
+        /// <inheritdoc />
         public XName Label => XName.Get("Label");
+        /// <inheritdoc />
         public XName Include => XName.Get("Include");
+        /// <inheritdoc />
         public XName Exclude => XName.Get("Exclude");
+        /// <inheritdoc />
         public XName Condition => XName.Get("Condition");
     }
 }
