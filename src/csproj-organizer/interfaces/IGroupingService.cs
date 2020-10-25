8a8f7ecfa253b17e8010098eaa4b94df440af7df
Rory Duncan <robust.rory@gmail.com>
2020-10-25 13:35:41 -0700

    Add xmldoc

--- src/csproj-organizer/interfaces/IGroupingService.cs
+++ src/csproj-organizer/interfaces/IGroupingService.cs
@@ -3,8 +3,17 @@ using CSProjOrganizer.Models;
 
 namespace CSProjOrganizer.Interfaces
 {
+    /// <summary>
+    /// A service for grouping the nodes within an XML document
+    /// </summary>
     public interface IGroupingService
     {
+        /// <summary>
+        /// Performs grouping on an XML document
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
         bool Group(XDocument document, SortOptions options);
     }
 }
