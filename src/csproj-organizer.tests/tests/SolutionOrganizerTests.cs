6a60e3efc00619de4894c167b1798a7ca9b753b6
Rory Duncan <robust.rory@gmail.com>
2020-10-31 17:46:30 -0700

    Add tests to SolutionOrganizerTests

--- src/csproj-organizer.tests/tests/SolutionOrganizerTests.cs
+++ src/csproj-organizer.tests/tests/SolutionOrganizerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using CSProjOrganizer.Interfaces;
@@ -15,13 +16,122 @@ namespace CSProjOrganizer.Tests
     {
 
         private readonly SolutionOrganizer _organizer;
+        private readonly Mock<ILogger<SolutionOrganizer>> _loggerMock;
+        private readonly Mock<IProjectOrganizer> _projectMock;
+
+        private readonly string solutionFile1 = "files/SolutionOrganizerTestsFile1.sln";
+        private readonly string solutionFile2 = "files/SolutionOrganizerTestsFile2.sln";
+        
 
         public SolutionOrganizerTests()
         {
-            var logger = Mock.Of<ILogger<SolutionOrganizer>>();
-            var projectOrganizer = Mock.Of<IProjectOrganizer>();
+            _loggerMock = new Mock<ILogger<SolutionOrganizer>>();
+            _projectMock = new Mock<IProjectOrganizer>();
+            _organizer = new SolutionOrganizer(_loggerMock.Object, _projectMock.Object);
+        }
 
-            _organizer = new SolutionOrganizer(logger, projectOrganizer);
+
+        [Fact]
+        public void HandlesInvalidPath()
+        {
+            _organizer.Run("dksangklds;nagklasd;mgldasgkasd");
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.AtLeastOnce());
+        }
+
+        [Fact]
+        public void HandlesNonExistantFile()
+        {
+            string cwd = Directory.GetCurrentDirectory();
+            Console.WriteLine(cwd);
+            _organizer.Run("not-real.sln");
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.AtLeastOnce());
+        }
+
+        [Fact]
+        public void HandlesRelativePathWithoutWarnings()
+        {
+            _organizer.Run(solutionFile1);
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Never);
+        }
+
+        [Fact]
+        public void HandlesAbsolutePath()
+        {
+            string cwd = Directory.GetCurrentDirectory();
+            _organizer.Run($"{cwd}/{solutionFile1}");
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Never);
+        }
+
+
+        [Fact]
+        public void HandlesSolutionHavingNonProjectDirectory()
+        {
+            _organizer.Run(solutionFile2);
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Exactly(1));
+        }
+
+        [Fact]
+        public void HandlesRelativePathWithoutErrors()
+        {
+            _organizer.Run(solutionFile1);
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Never);
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Critical,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Never);
+        }
+
+        [Fact]
+        public void OutputsMessagingForEachProject()
+        {
+            int projectsInSolution = 3;
+            _organizer.Run(solutionFile1);
+
+            _loggerMock.Verify(l => l.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.AtLeast(projectsInSolution));
         }
     }
 }
